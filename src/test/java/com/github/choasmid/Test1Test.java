6adef91bb8daed3c092e3e87e94359b5bc3eec9b
John Doe <johndoe@example.com>
2017-12-06 12:03:15 +0800

    none done

---
+++ src/test/java/com/github/choasmid/Test1Test.java
@@ -0,0 +1,13 @@
+package com.github.choasmid;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Test1Test {
+    @Test
+    public void getType() throws Exception {
+
+    }
+
+}
